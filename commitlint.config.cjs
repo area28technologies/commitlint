704bb30f331cf2ebdb26034e386ba8b32c43416a
Jacky Lau <jacky.lau@area28.io>
2022-10-26 11:47:07 +0800

    fix: set max line length to 72
    no issue

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  defaultIgnores: false
+  defaultIgnores: false,
+  rules: {
+    'header-max-length': [2, 'always', 72], // default which is overridden by the plugin
+  },
 };
